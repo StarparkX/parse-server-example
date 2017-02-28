12e384a6c26392ca3e4a7fb2f02c017218e6a4d9
mayos53 <yossi.assayag@gmail.com>
2017-02-28 14:46:43 +0200

    e

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '12345',
-  masterKey: process.env.MASTER_KEY || '12345', //Add your master key here. Keep it secret!
-  clientKey: '12345',
+  appId: process.env.APP_ID || '1j5AUs95Nx9y4fbZJEXZNiOVEjcbJjalTFbxMGFQ',
+  masterKey: process.env.MASTER_KEY || 'xRbUuRWOv21pvLmBddf9gTcmcnyX1R9AHkjHNRnb', //Add your master key here. Keep it secret!
+  clientKey: 'xRbUuRWOv21pvLmBddf9gTcmcnyX1R9AHkjHNRnb',
   serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
