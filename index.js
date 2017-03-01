20eeb3e480f3a64dfc10e278317b9c434dff2309
mayos53 <yossi.assayag@gmail.com>
2017-03-01 16:09:53 +0200

    rr

--- index.js
+++ index.js
@@ -69,9 +69,10 @@ var api = new ParseServer({
   }
 });
 
-if(user.language == 'en'){
-  api.passwordResetSubject = 'אסייג'
-}
+// if(user.language == 'en'){
+//   api.passwordResetSubject = 'אסייג'
+// }
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
